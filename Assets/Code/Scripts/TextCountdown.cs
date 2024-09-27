b2c64b1c1371beb42d90aab962889485680b8e53
Kronos <joaosantosprofi@protonmail.com>
2024-09-27 21:05:02 +0100

    base countdown text.

---
+++ Assets/Code/Scripts/TextCountdown.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class TextCountdown : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private TextMeshProUGUI _text;
+
+    private Animator _canvasAnimator;
+
+    private string _baseText;
+
+    private void OnEnable()
+    {
+        VideosManager.OnSectionEndCooldown.AddListener(ShowText);
+        VideosManager.OnSectionEndCooldown.AddListener(() => StartCoroutine(SetCountdownText()));
+        VideosManager.OnSectionEnd.AddListener(HideText);
+    }
+    private void OnDisable()
+    {
+        VideosManager.OnSectionEndCooldown.RemoveListener(ShowText);
+        VideosManager.OnSectionEndCooldown.RemoveListener(() => StartCoroutine(SetCountdownText()));
+        VideosManager.OnSectionEnd.RemoveListener(HideText);
+    }
+    private void Awake()
+    {
+        _canvasAnimator = GetComponent<Animator>();
+        _baseText = _text.text;
+    }
+    private void ShowText()
+    {
+        _canvasAnimator.SetBool("State", true);
+    }
+    private void HideText()
+    {
+        _canvasAnimator.SetBool("State", false);
+    }
+
+    private IEnumerator SetCountdownText()
+    {
+        for (int i = 5; i >= 0; i--)
+        {
+            yield return new WaitForSeconds(1);
+
+            _text.text = _baseText + " " + i.ToString();
+        }
+    }
+}
